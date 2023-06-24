1003ba1b77be9c6c12c8d50ca7391f71f6bc15c8
Vu Hung Thang <hungthang12b2@gmail.com>
2023-06-24 21:36:07 +0700

    addition function print_queue

--- queue.c
+++ queue.c
@@ -1,6 +1,36 @@
 #include <stdint.h>
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
+
+#define ELEMENT_QUEUE 100
+
+typedef uint32_t Type_queue; 
+
+
+typedef struct 
+{
+	uint32_t rear;
+	uint32_t front;
+	uint32_t size;
+	Type_queue Queue[ELEMENT_QUEUE];
+	
+}Queue_circular;
+
+void printf_queue(Queue_circular* Queue)
+
+{
+	uint32_t index = Queue->front;
+	uint32_t count = 0
+
+	while(index < Queue->size)
+	{
+		printf("%d ", Queue->front);
+		index = (index +1) % ELEMENT_QUEUE;
+		count++;
+	}
+	printf("/n");
+
+}
 int main()
 {
 	return 0;
